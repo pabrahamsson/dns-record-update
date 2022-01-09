306b0cbf500cb7dfcda6001561f993c8e81d8d03
Petter Abrahamsson <pabraham@redhat.com>
2022-01-09 07:57:36 -0500

    Code cleanup and improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate maplit;
-extern crate clap;
 extern crate cloudflare;
 extern crate env_logger;
 extern crate trust_dns_resolver;
@@ -11,11 +8,11 @@ use std::{
     fs,
     io::Write,
     net::{IpAddr, Ipv4Addr},
+    process,
     thread,
     time::Duration,
 };
 use chrono::Local;
-use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use cloudflare::endpoints::{dns, zone};
 use cloudflare::framework::{
     apiclient::ApiClient,
@@ -34,12 +31,22 @@ const LOOKUP_HOSTNAME: &str = "myip.opendns.com";
 const RESOLVER_ADDRESS: IpAddr = IpAddr::V4(Ipv4Addr::new(208, 67, 222, 222));
 const VAULT_ADDR: &str = "http://vault.vault.svc:8200";
 
-type SectionFunction<ApiClientType> = fn(&ArgMatches, &ApiClientType);
+struct Config {
+    zone: String,
+    record: String,
+}
 
-struct Section<'a, ApiClientType: ApiClient> {
-    args: Vec<Arg<'a, 'a>>,
-    description: &'a str,
-    function: SectionFunction<ApiClientType>,
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() != 3 {
+            return Err("Incorrect number of arguments");
+        }
+
+        let zone = args[1].clone();
+        let record = args[2].clone();
+
+        Ok(Config { zone, record })
+    }
 }
 
 #[derive(Serialize)]
@@ -51,38 +58,13 @@ struct LogMessage {
 
 #[derive(Deserialize)]
 #[allow(dead_code)]
-struct VaultAuthMetadata {
-    role: String,
-    service_account_name: String,
-    service_account_namespace: String,
-    service_account_secret_name: String,
-    service_account_uid: String,
+struct VaultAuth {
+    client_token: String,
 }
 
 #[derive(Deserialize)]
 #[allow(dead_code)]
-struct VaultAuth {
-    client_token: String,
-    accessor: String,
-    policies: Vec<String>,
-    token_policies: Vec<String>,
-    metadata: VaultAuthMetadata,
-    lease_duration: u32,
-    renewable: bool,
-    entity_id: String,
-    token_type: String,
-    orphan: bool,
-}
-#[derive(Deserialize)]
-#[allow(dead_code)]
 struct Lease {
-    request_id: String,
-    lease_id: String,
-    renewable: bool,
-    lease_duration: u32,
-    data: Option<String>,
-    wrap_info: Option<String>,
-    warnings: Option<Vec<String>>,
     auth: VaultAuth,
 }
 
@@ -123,22 +105,17 @@ fn print_type_of<T>(_: &T) {
 fn handle_cf_error(api_failure: &ApiFailure) {
     match api_failure {
         ApiFailure::Error(status, errors) => {
-            //println!("HTTP {}:", status);
             warn!("HTTP {}:", status);
             for err in &errors.errors {
-                //println!("Error {}: {}", err.code, err.message);
                 warn!("Error {}: {}", err.code, err.message);
                 for (k, v) in &err.other {
-                    //println!("{}: {}", k, v);
                     warn!("{}: {}", k, v);
                 }
             }
             for (k, v) in &errors.other {
-                //println!("{}: {}", k, v);
                 warn!("{}: {}", k, v);
             }
         }
-        //ApiFailure::Invalid(reqwest_err) => println!("Error: {}", reqwest_err),
         ApiFailure::Invalid(reqwest_err) => warn!("Error: {}", reqwest_err),
     }
 }
@@ -217,37 +194,25 @@ fn update_record<ApiClientType: ApiClient>(record_identifier: &str, zone_identif
     }
 }
 
-fn dns<ApiClientType: ApiClient>(arg_matches: &ArgMatches, api_client: &ApiClientType) {
-    let usage = "usage: dns ZONE_ID RECORD_NAME";
-
-    let record_name_missing = format!("missing '{}': {}", "RECORD_NAME", usage);
-    let record_name = arg_matches.value_of("record_name").expect(&record_name_missing);
-    let zone_name_missing = format!("missing '{}': {}", "ZONE_NAME", usage);
-    let zone_name = arg_matches.value_of("zone_name").expect(&zone_name_missing);
-
+fn dns<ApiClientType: ApiClient>(zone_name: &str, record_name: &str, api_client: &ApiClientType) {
     let current_ip = dns_lookup(&vec![RESOLVER_ADDRESS], LOOKUP_HOSTNAME);
-    let lookup_ip = dns_lookup(CLOUDFLARE_IPS, &record_name);
+    let lookup_ip = dns_lookup(CLOUDFLARE_IPS, record_name);
     if &current_ip == &lookup_ip {
-        //println!("DNS record for {} ({}) is up to date",
         info!("DNS record for {} ({}) is up to date",
-            &record_name,
+            record_name,
             &lookup_ip.unwrap())
     } else {
-        //println!("DNS record for {} ({} ==> {}) will be updated",
         info!("DNS record for {} ({} ==> {}) will be updated",
-            &record_name,
+            record_name,
             &lookup_ip.unwrap(),
             &current_ip.unwrap());
-        let zone_identifier = get_zone_id(&zone_name, api_client).unwrap();
-        let record_id = get_current_record(&record_name, &zone_identifier, api_client).unwrap();
-        update_record(&record_id, &zone_identifier, &record_name, &current_ip, api_client);
+        let zone_identifier = get_zone_id(zone_name, api_client).unwrap();
+        let record_id = get_current_record(record_name, &zone_identifier, api_client).unwrap();
+        update_record(&record_id, &zone_identifier, record_name, &current_ip, api_client);
     }
 }
 
 fn get_vault_token() -> Result<String, ureq::Error> {
-    //let default_jwt_token_path: Result<String, String> = Ok(String::from(JWT_TOKEN_PATH));
-    //let default_vault_addr: Result<String, String> = Ok(String::from(VAULT_ADDR));
-    //let jwt_token_path = env::var("JWT_TOKEN_PATH").or(default_jwt_token_path).unwrap();
     let jwt_token_path = env::var("JWT_TOKEN_PATH").ok();
     let jwt_token_path = jwt_token_path
         .as_ref()
@@ -258,6 +223,7 @@ fn get_vault_token() -> Result<String, ureq::Error> {
     let vault_addr = vault_addr
         .as_ref()
         .map(String::as_str)
+        .and_then(|s| if s.is_empty() { None } else { Some(s) })
         .unwrap_or(VAULT_ADDR);
     let jwt = fs::read_to_string(jwt_token_path)?;
     let mount = "kubernetes";
@@ -275,8 +241,12 @@ fn get_vault_token() -> Result<String, ureq::Error> {
 }
 
 fn get_cf_api_key(token: &str) -> Result<String, ureq::Error> {
-    let default_vault_addr: Result<String, String> = Ok(String::from("http://vault.vault.svc:8200"));
-    let vault_addr = env::var("VAULT_ADDR").or(default_vault_addr).unwrap();
+    let vault_addr = env::var("VAULT_ADDR").ok();
+    let vault_addr = vault_addr
+        .as_ref()
+        .map(String::as_str)
+        .and_then(|s| if s.is_empty() { None } else { Some(s) })
+        .unwrap_or(VAULT_ADDR);
     let vault_secret_endpoint = format!("{0}/v1/kv/data/cf-dns-gtvc-net-api", vault_addr);
     let response: VaultKV2 = ureq::get(&vault_secret_endpoint)
         .set("X-Vault-Token", token)
@@ -286,7 +256,12 @@ fn get_cf_api_key(token: &str) -> Result<String, ureq::Error> {
 }
 
 //fn main() -> Result<(), Box<dyn std::error::Error>> {
-fn main(){
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        error!("Failed to parse arguments: {}", err);
+        process::exit(1)
+    });
     Builder::new()
         .format(|buf, record| {
             let log_message = LogMessage {
@@ -297,116 +272,26 @@ fn main(){
             writeln!(buf,
                 "{}",
                 serde_json::to_string(&log_message).unwrap()
-                /*
-                serde_json::json!({
-                    "timestamp": format!("{}", Local::now().format("%Y-%m-%dT%H:%M:%S")),
-                    "loglevel": record.level().as_str(),
-                    "message": record.args()
-                }).to_string()
-                */
             )
-            /*
-            writeln!(buf,
-                "{} [{}] - {}",
-                Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                record.level(),
-                record.args()
-            )
-            */
         })
         .filter(None, LevelFilter::Info)
         .init();
     loop {
-        let sections = hashmap! {
-            "dns" => Section{
-                args: vec![
-                    Arg::with_name("zone_name").required(true),
-                    Arg::with_name("record_name").required(true),
-                ],
-                description: "DNS records for a zone",
-                function: dns
-            }
-        };
+        let vault_token = get_vault_token().expect("Failed to get Vault token");
+        let cf_key = get_cf_api_key(&vault_token).expect("Failed to get CF api key");
 
-        let vault_token = get_vault_token();
-        let cf_key = get_cf_api_key(&vault_token.unwrap());
-
-        let mut cli = App::new("cf-rust")
-            .version("0.1")
-            .author("Petter Abrahamsson <petter@jebus.nu>")
-            .about("Tiny Cloudflare API client")
-            .arg(Arg::with_name("email")
-                .long("email")
-                .help("Email address associated with your account")
-                .takes_value(true)
-                .requires("auth-key"))
-            .arg(Arg::with_name("auth-key")
-                .long("auth-key")
-                .env("CF_RS_AUTH_KEY")
-                .help("API key generated on the \"My Account\" page")
-                .takes_value(true)
-                .requires("email"))
-            .arg(Arg::with_name("auth-token")
-                .long("auth-token")
-                .env("CF_RS_AUTH_TOKEN")
-                .help("API token generated on the \"My Account\" page")
-                .takes_value(true)
-                .conflicts_with_all(&["email", "auth-key"]))
-            .setting(AppSettings::ArgRequiredElseHelp);
-
-        for (section_name, section) in sections.iter() {
-            let mut subcommand = SubCommand::with_name(section_name).about(section.description);
-
-            for arg in &section.args {
-                subcommand = subcommand.arg(arg);
-            }
-            cli = cli.subcommand(subcommand);
-        }
-
-        let matches = cli.get_matches();
-        let matched_sections =
-            sections
-                .iter()
-                .filter(|&(section_name, _): &(&&str, &Section<HttpApiClient>)| {
-                    matches.subcommand_matches(section_name).is_some()
-                });
-
-        let email = matches.value_of("email");
-        let key = matches.value_of("auth-key");
-        //let token = matches.value_of("auth-token");
-        let token = Some(cf_key.as_ref().unwrap().trim_matches('"'));
-
-        let credentials: Credentials = if let Some(key) = key {
-            Credentials::UserAuthKey {
-                email: email.unwrap().to_string(),
-                key: key.to_string(),
-            }
-        } else if let Some(token) = token {
-            Credentials::UserAuthToken {
-                token: token.to_string(),
-            }
-        } else {
-            panic!("Either API token or API key + email pair must be provided")
+        let credentials: Credentials = Credentials::UserAuthToken {
+            token: cf_key.trim_matches('"').to_string(),
         };
 
         let api_client = HttpApiClient::new(
             credentials,
             HttpApiClientConfig::default(),
             Environment::Production,
-        //)?;
         ).unwrap();
 
-        for (section_name, section) in matched_sections {
-            (section.function)(
-                matches.subcommand_matches(section_name).unwrap(),
-                &api_client,
-            );
-        }
+        dns(&config.zone, &config.record, &api_client);
 
-        //zone(matches.subcommand_matches("zone").unwrap(), &api_client);
-        //list_zone(matches.subcommand_matches("zone").unwrap(), &api_client);
-
-        //Ok(())
         thread::sleep(Duration::from_secs(120))
     }
 }
